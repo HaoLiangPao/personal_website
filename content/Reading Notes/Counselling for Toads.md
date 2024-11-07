a6424416863f51d5a72bd8477d2e25839e7d8456
Hao Liang <Hao.Liang@ibm.com>
2024-11-07 14:40:06 -0500

    vault backup: 2024-11-07 14:40:06

---
+++ content/Reading Notes/Counselling for Toads.md
@@ -0,0 +1,10 @@
+---
+tags:
+  - Reading
+  - Book
+  - Psycho
+  - Self_development
+---
+
+《蛤蟆先生去看心理医生》/ 《Counselling for Toads: A Psychological Adventure 》
+
--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🪴 Hao's the Future?",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "haoliangpao.github.io/personal_website/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
